5a0d0fdf56e58e826b7ed1195f77f284094df541
PRE.AX <preacc.company@gmail.com>
2021-02-20 15:27:35 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './assets/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          PRE.AX Марафон
+        </p>
+        <p>
+          Я обязательно научусь работать с GitHub! Даже не надейтесь...Ха-ха-ха
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
