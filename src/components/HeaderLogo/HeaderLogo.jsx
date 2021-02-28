f4483b44bc2df2e31ddc67e7d65d7df891fb839b
Sergei Fomin <sergeifomin83@inbox.ru>
2021-02-28 20:41:35 +0300

    fixed alt in logo

--- src/components/HeaderLogo/HeaderLogo.jsx
+++ src/components/HeaderLogo/HeaderLogo.jsx
@@ -7,7 +7,7 @@ export const HeaderLogo = ({ title }) => {
   return (
     <div className={styles.logo}>
       <div className={styles.icon}>
-        <img src={icon} alt="title"/>
+        <img src={icon} alt={title}/>
       </div>
       <h1 className={styles.text}>{title}</h1>
     </div>
